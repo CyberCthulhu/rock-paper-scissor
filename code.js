32c6504f1b945d7b8e3cdc8884d3436490bc7508
CyberCthulhu <csmorales2002@gmail.com>
2023-07-26 01:48:00 -0500

    completed all game functions required to play

--- code.js
+++ code.js
@@ -1,3 +1,66 @@
 let move= prompt("Choose your move")
 
-console.log(move)
+
+function playerChoice(str){
+    str = str.toLowerCase()
+
+    if(str ="rock"){
+        return 1
+    }
+    else if(str="paper"){
+        return 2
+    }
+    else if(str="scissors"){
+        return 3
+    }
+}
+let playerMove =playerChoice(move)
+function getComputerChoice(){
+    return Math.floor(Math.random()*(3-1+1)+1)
+}
+
+let CompChoice= getComputerChoice()
+
+
+function playRound(pMove,cMove){
+    if(pMove ===1 && cMove=== 1){
+        document.write("Draw")
+        return
+    }
+    else if(pMove === 1 && cMove ==2){
+        document.write("Computer wins")
+        return
+    }
+    else if(pMove === 1 && cMove == 3){
+        document.write("Player wins")
+        return
+    }
+    else if(pMove === 2 && cMove ===1){
+        document.write("Player wins")
+        return
+    }
+    else if(pMove ===2 && cMove ===2){
+        document.write("Draw")
+        return
+    }
+    else if(pMove === 2 && cMove ===3){
+        document.write("Computer wins")
+        return
+    }
+    else if(pMove ===3&& cMove ===1){
+        document.write("Computer wins")
+        return
+    }
+    else if(pMove=== 3 && cMove ===2){
+        document.write("Player wins")
+        return
+    }
+    else if(pMove===3 && cMove===3){
+        document.write("Draw")
+        return
+    }
+}
+
+playRound(playerMove,CompChoice)
+
+
